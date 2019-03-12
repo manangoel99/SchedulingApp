eba7ac79106bea3d2db350a68e54fd5f9a266714
Manan Goel <manan.goel@research.iiit.ac.in>
2019-03-12 07:05:18 +0530

    Basics Implemented

---
+++ SchedulingApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchedulingappConfig(AppConfig):
+    name = 'SchedulingApp'
---
+++ SchedulingApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    datetime = models.DateTimeField()
+    description = models.CharField(max_length=300)
+    venue = models.CharField(max_length=200)
