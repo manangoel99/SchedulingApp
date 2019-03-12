2ef20cfd77d2dad3703ac050d94877b0ce75ee81
Manan Goel <manan.goel@research.iiit.ac.in>
2019-03-12 07:11:24 +0530

    Sign Up implemented

--- SchedulingApp/views.py
+++ SchedulingApp/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola")
+    if request.method == "POST":
+        if "SignIn" in request.POST:
+            print(request.POST)
+        if "SignUp" in request.POST:
+            u = User(username=request.POST["username"], first_name=request.POST["name"], password=request.POST["password"], email=request.POST["email"])
+            User.save(u)
+            login(request, u)
+            return redirect(PersonalPage, u.pk)
+    return render(request, template_name="index.html")
+
+@login_required(login_url="")
+def PersonalPage(request, id):
+    return render(request, template_name="UserHome.html")
